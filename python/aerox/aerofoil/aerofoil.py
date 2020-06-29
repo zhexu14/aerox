7915c58c9b023939e462549b710dedc4d153a47e
Zhe Xu <zhexu14@gmail.com>
2020-06-29 16:13:50 +1000

    python/aerox: clean up, add pydoc

--- python/aerox/aerofoil/aerofoil.py
+++ python/aerox/aerofoil/aerofoil.py
@@ -3,15 +3,17 @@ import numpy as np
 
 
 class Aerofoil:
-
-    def __init__( self ):
+    """
+    Representation of an aerofoil geometry.
+    """
+    def __init__(self):
         self.coordinates = []
         self.top = []
         self.bottom = []
         self.leading_edge = None
         self.trailing_edge = None
 
-    def load_from_gnu( self, file ):
+    def load_from_gnu(self, file):
         """
         Load aerofoil from gnu format output
         :param file: file-like object to read from
@@ -21,23 +23,35 @@ class Aerofoil:
         bottom = []
         points = top
         for line in file:
-            line = line.rstrip( '\n' )
-            line = line[ 3: ]
-            s = line.split( ' ' )
-            if len( s ) > 2:
-                s = [ s[ 0 ], s[ -1 ] ]
-            if len( s ) < 2:
+            line = line.rstrip('\n')
+            line = line[3:]
+            s = line.split(' ')
+            if len(s) > 2:
+                s = [s[0], s[-1]]
+            if len(s) < 2:
                 points = bottom
                 continue
-            points.append( ( float( s[ 0 ] ), float( s[ 1 ] ) ) )
+            points.append((float(s[0]), float(s[1])))
 
         bottom.reverse()
-        self.coordinates = top + bottom[ 1: -1 ]
-        self.top = top[ 1: -1 ]
-        self.bottom = bottom[ 1: -1 ]
-        self.leading_edge = top[ 0 ]
-        self.trailing_edge = top[ -1 ]
+        self.coordinates = top + bottom[1: -1]
+        self.top = top[1: -1]
+        self.bottom = bottom[1: -1]
+        self.leading_edge = top[0]
+        self.trailing_edge = top[-1]
 
-    def plot( self ):
-        c = np.array(  self.coordinates )
-        plt.plot( c[ :, 0 ], c[ :, 1 ] )
+    def plot(self):
+        """
+        Plot aerofoil geometry using matplotlib.
+
+        Example:
+        >>> import matplotlib.pyplot as plt
+        >>> aerofoil = Aerofoil()
+        >>> with open('coordinates.gnu', 'r') as file:
+        >>>     aerofoil.load_from_gnu(file)
+        >>> aerofoil.plot()
+        >>> plt.show()
+        :return: None
+        """
+        c = np.array(self.coordinates)
+        plt.plot(c[:, 0], c[:, 1])
--- python/aerox/drivers/gmsh/driver.py
+++ python/aerox/drivers/gmsh/driver.py
@@ -2,32 +2,41 @@ import os
 import subprocess
 import shlex
 
+
 def default_config():
     """
     - path: absolute path where gmsh executable is, or None if gmsh is installed (i.e. in PATH environment variable)
     - dimensions: 1, 2, or 3
+    - working_directory: working directory path as str.
     :return: default config as dict
     """
-    return { 'path': None,
-             'dimensions': 2,
-             'working_directory': '.' }
+    return {'path': None,
+            'dimensions': 2,
+            'working_directory': '.'}
 
-def run( geometry, config ):
-    geometry_file = os.path.join( config[ 'working_directory' ], 'test.geo' )
+
+def run(geometry, config):
+    """
+    Runs gmsh to generate SU2 mesh.
+    :param geometry: gmsh geometry definition (contents of .geo file) as string.
+    :param config: gmsh config, see default_config() for details.
+    :return: None
+    """
+    geometry_file = os.path.join(config['working_directory'], 'mesh.geo')
     output = os.path.join(config['working_directory'], 'mesh.su2')
-    with open( geometry_file, 'w' ) as fd:
+    with open(geometry_file, 'w') as fd:
         for line in geometry:
-            fd.write( line + '\n' )
+            fd.write(line + '\n')
 
     executable = 'gmsh'
-    if config[ 'path' ] is not None:
+    if config['path'] is not None:
         executable = config[ 'path' ]
 
-    command = '{executable} -{dimensions} {geometry} -format su2 -o {output}'.format( executable = executable,
-                                                                                      dimensions = config[ 'dimensions' ],
-                                                                                      geometry = geometry_file,
-                                                                                      output = output )
-    p = subprocess.Popen( shlex.split( command ),
-                          stdin = subprocess.PIPE,
-                          stdout = subprocess.PIPE )
+    command = '{executable} -{dimensions} {geometry} -format su2 -o {output}'.format(executable = executable,
+                                                                                     dimensions = config['dimensions'],
+                                                                                     geometry = geometry_file,
+                                                                                     output = output)
+    p = subprocess.Popen(shlex.split(command),
+                         stdin = subprocess.PIPE,
+                         stdout = subprocess.PIPE)
     p.wait()
