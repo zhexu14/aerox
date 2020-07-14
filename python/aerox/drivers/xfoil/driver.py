bca0507601f8ce54d73635cb3f62332d1eec63cd
Zhe Xu <zhexu14@gmail.com>
2020-07-14 21:41:19 +1000

    python/aerox/drivers/xfoil/driver.py: minor clean up

--- python/aerox/drivers/xfoil/driver.py
+++ python/aerox/drivers/xfoil/driver.py
@@ -1,9 +1,12 @@
+import numpy as np
 import xfoil
+
 
 def default_config():
     """
-    - path: path to xfoil executable. If none, use xfoil i.e. assume executable is in PATH
     - alphas: list of alphas to evaluate
+    - xfoil/reynolds_number: Reynolds number
+    - xfoil/mach: freestream Mach number
     :return: default config as dict
     """
     config = {}
@@ -12,22 +15,31 @@ def default_config():
                        'mach': 0.0 }
     return config
 
+
 def run(config, aerofoil):
     """
-
-    :param config:
-    :param aerofoil:
-    :return:
+    Run xfoil analysis.
+    :param config: config as dict, see default_config() for details.
+    :param aerofoil: aerox.aerofoil.aerofoil.Aerofoil object.
+    :return: list of length equal to the number of alphas to evaluate. Entries are either None (analysis failed to
+             converge) or dict with the following keys:
+             - lift: lift coefficient
+             - drag: drag coefficing
+             - pitching_moment: quarter-chord pitching moment coefficient
     """
     xf = xfoil.XFoil()
     xf.airfoil = aerofoil.to_xfoil_airfoil()
-    xf.repanel()
     xf.Re = float(config['xfoil']['reynolds_number'])
     xf.M = config['xfoil']['mach']
+    xf.max_iter = 100
     r = []
     for alpha in config['alphas']:
+        xf.reset_bls()
         cl, cd, cm, cp = xf.a(alpha)
-        r.append({'lift': cl,
-                  'drag': cd,
-                  'pitching_moment': cm})
+        if np.isnan(cl):
+            r.append(None)
+        else:
+            r.append({'lift': cl,
+                      'drag': cd,
+                      'pitching_moment': cm})
     return r
